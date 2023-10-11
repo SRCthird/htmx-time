155b8f72dc9f6a295db46cc1f0c0df6ab3f70efe
SRCthird <SRCthird@gmail.com>
2023-10-11 17:57:12 -0400

    Abstract head/body

--- elements.js
+++ elements.js
@@ -1,4 +1,13 @@
-export const root = (child) => {
+/**
+ * This is the root HTML of the HTMX application.
+ * 
+ * @param {JSX} head - The HTML code for the head of the root page.
+ * @param {JSX} body - The HTML code for the body of the root page.
+ * @returns {string} - Returns the HTML code for the root of .
+ */
+export const root = (head, body) => {
+    if (!head) head = '<title>HTMX with Node.js</title>';
+    if (!body) body = '';
     return(
         `<!DOCTYPE html>
         <html lang="en">
@@ -6,10 +15,10 @@ export const root = (child) => {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <script src="https://unpkg.com/htmx.org@1.6.1"></script>
-            <title>HTMX with Node.js</title>
+            ${head}
         </head>
         <body>
-            ${child}
+            ${body}
         </body>
         </html>`
     );
