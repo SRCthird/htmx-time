816de478f2a1044f15f58469e6185e8245aefadc
Stephen Chryn <SRCthird@gmail.com>
2024-10-09 12:48:59 -0400

    Make the function more readable

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,20 @@
 import { TemplateResult } from "lit-html";
 
-export const render = (data: TemplateResult<1>): string => {
-  const {strings, values} = data;
-  const v = [...values, ''].map(e => typeof e === 'object' ? render(e as TemplateResult<1>) : e )      
-  return strings.reduce((acc,s, i) => acc + s + v[i], '')
-}
+/**
+ * Renders a TemplateResult object to a string.
+ * 
+ * @param template - The TemplateResult object containing the template strings and values.
+ * @returns The fully rendered string.
+ */
+export const render = (template: TemplateResult<1>): string => {
+  const { strings, values } = template;
+  
+  const resolvedValues = [...values, ''].map(value => 
+    typeof value === 'object' ? render(value as TemplateResult<1>) : value 
+  );
+
+  return strings.reduce((result, currentString, index) => 
+    result + currentString + resolvedValues[index], 
+    ''
+  );
+};
